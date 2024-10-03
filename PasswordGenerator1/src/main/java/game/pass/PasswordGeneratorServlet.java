d30b13a02eaad75c7d6588ee1003ee933417ecbd
Kalavathi S <kalavathi27145@gmail.com>
2024-10-03 18:07:20 +0530

    initial commit

---
+++ PasswordGenerator1/src/main/java/game/pass/PasswordGeneratorServlet.java
@@ -0,0 +1,33 @@
+package game.pass;
+
+import java.io.IOException;
+import java.security.SecureRandom;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/generatePassword")
+public class PasswordGeneratorServlet extends HttpServlet {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int length = Integer.parseInt(request.getParameter("length"));
+        response.getWriter().write(generateRandomPassword(length));
+    }
+
+    private String generateRandomPassword(int length) {
+        SecureRandom random = new SecureRandom();
+        StringBuilder password = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(CHARACTERS.length());
+            password.append(CHARACTERS.charAt(index));
+        }
+
+        return password.toString();
+    }
+}
